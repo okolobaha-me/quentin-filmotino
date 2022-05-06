4947cbfb0af294b4b201101cda1e7dcfc5b817d4
Anna Miklukha <miklukha.anna@ukr.net>
2022-05-06 15:14:24 +0200

    almost finish modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,21 +1,15 @@
-// const refs = {
-//   openModalCardBtn: document.querySelector('[data-modal-card-open]'),
-//   openModalBtn: document.querySelector('[data-modal-open]'),
-//   closeModalBtn: document.querySelector('[data-modal-close]'),
-//   modal: document.querySelector('[data-modal]'),
-//   modalCard: document.querySelector('[data-modal-card]'),
-//   body: document.querySelector('body'),
-// };
-
 const refs = {
   openModalBtn: document.querySelectorAll('[data-modal-open]'),
   closeModalBtn: document.querySelectorAll('[data-modal-close]'),
+  modal: document.querySelector('[data-modal]'),
+  body: document.querySelector('body'),
 };
 
 refs.closeModalBtn.forEach(btn => {
   btn.addEventListener('click', () => {
     const backdrop = btn.closest('[data-modal]');
     backdrop.classList.add('is-hidden');
+    bodyUnlock();
   });
 });
 
@@ -28,12 +22,26 @@ refs.openModalBtn.forEach(btn => {
         return;
       }
     }
-
+    bodyLock()
     const backdrop = document.querySelector(btn.dataset.target);
     backdrop.classList.remove('is-hidden');
   });
 });
 
-// забрати можливість скролити
-// табом проходитися
-// однакова модалка
+window.onclick = event => {
+  if (event.target === refs.modal) {
+    refs.modal.classList.add('is-hidden');
+    bodyUnlock()
+  }
+};
+
+function bodyLock(){
+  // const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+  // refs.body.style.paddingRight = lockPaddingValue;
+	refs.body.classList.add('lock');
+}
+
+function bodyUnlock(){
+  // refs.body.style.paddingRight = '0px';
+  refs.body.classList.remove('lock');
+}
