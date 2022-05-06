97e97d8a339805c7bd5ff6afa3d734baff52d1d8
Anna Miklukha <miklukha.anna@ukr.net>
2022-05-06 13:03:54 +0200

    change modal code

--- src/js/modal.js
+++ src/js/modal.js
@@ -3,21 +3,40 @@ const refs = {
   openModalBtn: document.querySelector('[data-modal-open]'),
   closeModalBtn: document.querySelector('[data-modal-close]'),
   modal: document.querySelector('[data-modal]'),
+  modalCard: document.querySelector('[data-modal-card]'),
+  body: document.querySelector('body'),
 };
 
-refs.openModalCardBtn.addEventListener('click', e => {
-  const cardItem = e.target.closest('.filmList__item');
-  if (cardItem) {
-    addModal();
+const values = Object.values(refs);
+
+if (!values.includes(null)) {
+  refs.openModalCardBtn.addEventListener('click', e => {
+    const cardItem = e.target.closest('.filmList__item');
+    if (cardItem) {
+      refs.modalCard.classList.remove('is-hidden');
+      // addModal();
+    }
+  });
+  refs.openModalBtn.addEventListener('click', addModal);
+  refs.closeModalBtn.addEventListener('click', removeModal);
+
+  function addModal() {
+    // if (refs.modal) {
+    refs.modal.classList.remove('is-hidden');
+    // }
   }
-});
-refs.openModalBtn.addEventListener('click', addModal);
-refs.closeModalBtn.addEventListener('click', removeModal);
 
-function addModal() {
-  refs.modal.classList.remove('is-hidden');
+  function removeModal() {
+    // if (refs.modal) {
+    refs.modal.classList.add('is-hidden');
+    // }
+  }
 }
 
-function removeModal() {
-  refs.modal.classList.add('is-hidden');
-}
+// function bodyLock() {
+//   const lockPaddingValue = window.innerWidth - modal.offsetWidth + 'px';
+// }
+
+// забрати можливість скролити
+// табом проходитися
+// однакова модалка
