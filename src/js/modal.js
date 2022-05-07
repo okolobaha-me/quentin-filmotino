b352ada6ff73dfdb33dda7d415200009f9b8f805
Anna Miklukha <miklukha.anna@ukr.net>
2022-05-07 14:51:16 +0200

    fix bag with offset modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,9 +1,44 @@
 const refs = {
-  openModalBtn: document.querySelectorAll('[data-modal-open]'),
+  openModalButtons: document.querySelectorAll('[data-modal-open]'),
   closeModalBtn: document.querySelectorAll('[data-modal-close]'),
-  modal: document.querySelector('[data-modal]'),
+  modals: document.querySelectorAll('[data-modal]'),
   body: document.querySelector('body'),
+  team: document.querySelector('.modal-team'),
+  film: document.querySelector('.film-modal'),
 };
+
+// function closeModal(){
+
+//   refs.closeModalBtn.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//       const backdrop = btn.closest('[data-modal]');
+//       backdrop.classList.add('is-hidden');
+//       bodyUnlock();
+//     });
+//   });
+
+//   window.onclick = event => {
+//     refs.modals.forEach(modal => {
+//       if (event.target === modal) {
+//         if (!modal.classList.contains('is-hidden')) {
+//           modal.classList.add('is-hidden');
+//           bodyUnlock();
+//         }
+//       }
+//     });
+//   };
+
+//   function onKeypress(e) {
+//     if (e.code === 'Escape') {
+//       refs.modals.forEach(modal => {
+//         if (!modal.classList.contains('is-hidden')) {
+//           modal.classList.add('is-hidden');
+//           bodyUnlock();
+//         }
+//       });
+//     }
+//   }
+// }
 
 refs.closeModalBtn.forEach(btn => {
   btn.addEventListener('click', () => {
@@ -13,35 +48,60 @@ refs.closeModalBtn.forEach(btn => {
   });
 });
 
-refs.openModalBtn.forEach(btn => {
-  btn.addEventListener('click', e => {
-    if (btn.nodeName === 'UL') {
+refs.openModalButtons.forEach(button => {
+  button.addEventListener('click', e => {
+    if (button.nodeName === 'UL') {
       const cardItem = e.target.closest('.filmList__item');
 
       if (!cardItem) {
         return;
       }
     }
-    bodyLock();
-    const backdrop = document.querySelector(btn.dataset.target);
-    backdrop.classList.remove('is-hidden');
+    openModal(button);
   });
 });
 
+function openModal(button) {
+  const backdrop = document.querySelector(button.dataset.target);
+  backdrop.classList.remove('is-hidden');
+  bodyLock();
+}
+
 window.onclick = event => {
-  if (event.target === refs.modal) {
-    refs.modal.classList.add('is-hidden');
-    bodyUnlock();
-  }
+  refs.modals.forEach(modal => {
+    if (event.target === modal) {
+      if (!modal.classList.contains('is-hidden')) {
+        modal.classList.add('is-hidden');
+        bodyUnlock();
+      }
+    }
+  });
 };
 
 function bodyLock() {
   const lockPaddingValue = window.innerWidth - refs.body.offsetWidth + 'px';
+
+  // refs.modals.forEach(modal => {
+  //   if (!modal.classList.contains('is-hidden')) {
+  //     modal.style.paddingRight = lockPaddingValue;
+  //   }
+  // });
+  refs.team.style.paddingRight = lockPaddingValue;
+  // refs.film.style.paddingRight = lockPaddingValue;
+
   refs.body.style.paddingRight = lockPaddingValue;
   refs.body.classList.add('lock');
 }
 
 function bodyUnlock() {
+  // refs.modals.forEach(modal => {
+  //   if (modal.classList.contains('is-hidden')) {
+  //     modal.style.paddingRight = '0px';
+  //   }
+  // });
+  refs.team.style.paddingRight = '0px';
+  // refs.film.style.paddingRight = '0px';
+
   refs.body.style.paddingRight = '0px';
   refs.body.classList.remove('lock');
 }
@@ -50,7 +110,14 @@ window.addEventListener('keydown', onKeypress);
 
 function onKeypress(e) {
   if (e.code === 'Escape') {
-    refs.modal.classList.add('is-hidden');
-    bodyUnlock();
+    refs.modals.forEach(modal => {
+      if (!modal.classList.contains('is-hidden')) {
+        modal.classList.add('is-hidden');
+        bodyUnlock();
+      }
+    });
   }
 }
+
+// добавити клас lock і залишити id="team-modal" почистити слухача
+// 64 рядочки
