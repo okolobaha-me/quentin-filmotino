e05fe94b3506a92b0eb850e1366d2a8420dc0c14
InM1nd <zabolotny1111@gmail.com>
2022-05-12 00:10:27 +0200

    работает но очень криво

--- src/js/render/main-render-logic.js
+++ src/js/render/main-render-logic.js
@@ -1,11 +1,7 @@
 import refs from './refs';
 
 import ApiService from '../API/api-service';
-
-//
-const API_KEY = 'api_key=79fb62b7e77dc5ee41dd0c1332d74198';
-const BASE_URL = 'https://api.themoviedb.org/3';
-const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' +API_KEY;
+import {convertIdInGenre} from '../API/convertIdtoGenre';
 const IMG_URL = 'https://image.tmdb.org/t/p/w500'
 // import renderFilmList from './render-film-list.js';
 
@@ -38,7 +34,6 @@ function onSiteLoad(e) {
     resetMarkup();
     refs.galleryRef.innerHTML = '<h1>здесь будут популярные за день фильмы ;)</h1>'
     console.log('Фильмы, приходящие, при загрузке страницы');
-
     getFilmsByUrl('/3/trending/all/day'); 
     service.getPopularFilms().then(data => {
       if (data.total_results === 0) {
@@ -49,6 +44,45 @@ function onSiteLoad(e) {
   });
 };
 
+function showMovies(data){
+  main.innerHTML = '';
+
+  data.forEach(movie => {
+      const {title, poster_path, vote_average, release_date, genre_ids} = movie;
+
+      let movieGenres = [];
+      for (let i = 0; i < genre_ids.length; i += 1) {
+      let genre = convertIdInGenre(genre_ids[i]);
+      movieGenres.push(genre);
+      };
+
+
+      const movieEl = document.createElement('li');
+      movieEl.classList.add('filmList__item');
+      movieEl.innerHTML = `
+      <div class="filmList__link">
+        <div class="filmList__poster">
+          <picture>
+            <img
+              src="${IMG_URL+poster_path}"
+              alt="${title}"
+            />
+          </picture>
+        </div>
+        <h2 class="filmList__title">${title}</h2>
+        <div class="filmList__info">
+          <p class="filmList__text">
+            <span class="filmList__genge">${movieGenres}</span> |
+            <span class="filmList__releaseDate">${release_date}</span>
+          </p>
+          <p class="filmList__voteAverage">${vote_average}</p>
+        </div>
+      </div>
+      `
+
+      main.appendChild(movieEl);
+  })
+}
 
 function getFilmsByUrl(url) {
     service.urlPath = url;
@@ -59,44 +93,8 @@ function getFilmsByUrl(url) {
 };
 
 
-
-
-
 function resetMarkup() {
     refs.galleryRef.innerHTML = '';
 };
 
 
-getMovies(API_URL);
-
-function showMovies(data){
-    main.innerHTML = '';
-
-    data.forEach(movie => {
-        const {title, poster_path, vote_average, release_date, genre_ids} = movie;
-        const movieEl = document.createElement('li');
-        movieEl.classList.add('filmList__item');
-        movieEl.innerHTML = `
-        <div class="filmList__link">
-          <div class="filmList__poster">
-            <picture>
-              <img
-                src="${IMG_URL+poster_path}"
-                alt="${title}"
-              />
-            </picture>
-          </div>
-          <h2 class="filmList__title">${title}</h2>
-          <div class="filmList__info">
-            <p class="filmList__text">
-              <span class="filmList__genge">${genre_ids}</span> |
-              <span class="filmList__releaseDate">${release_date}</span>
-            </p>
-            <p class="filmList__voteAverage">${vote_average}</p>
-          </div>
-        </div>
-        `
-
-        main.appendChild(movieEl);
-    })
-}
