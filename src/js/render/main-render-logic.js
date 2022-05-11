b80786e29cbe7ce0e4722b3cecd318aba9b054b8
BorisKorsun <mainboko@gmail.com>
2022-05-11 15:40:46 +0300

    api-option

--- src/js/render/main-render-logic.js
+++ src/js/render/main-render-logic.js
@@ -19,6 +19,7 @@ function onFormSubmit(e) {
     
     resetMarkup();
     console.log(`Фильмы по запросу ${query}:`);
+    refs.galleryRef.innerHTML = '<h1>здесь будут фильмы по запросу ;)</h1>'
     service.getFilmsByQuery(query).then(data => {
         if (data.total_results === 0) {
             console.log('запросов не найдено')
