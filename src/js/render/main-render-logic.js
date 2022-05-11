c8c0095acb7bb9e93f8f80b8ad981c62ab83a74c
BorisKorsun <mainboko@gmail.com>
2022-05-11 15:28:13 +0300

    adds-input-validation

--- src/js/render/main-render-logic.js
+++ src/js/render/main-render-logic.js
@@ -12,17 +12,33 @@ refs.formRef.addEventListener('submit', onFormSubmit);
 function onFormSubmit(e) {
     e.preventDefault();
     const query = e.target.elements.searchFilm.value;
+
+    if (query.trim() === '') {
+        return;
+    }
     
     resetMarkup();
-    console.log('Фильмы по запросу');
-    service.getFilmsByQuery(query).then(console.log);
+    console.log(`Фильмы по запросу ${query}:`);
+    service.getFilmsByQuery(query).then(data => {
+        if (data.total_results === 0) {
+            console.log('запросов не найдено')
+            return
+        };
+        console.log(data)
+    });
 }
 
 function onSiteLoad(e) {
     resetMarkup();
     refs.galleryRef.innerHTML = '<h1>здесь будут популярные за день фильмы ;)</h1>'
     console.log('Фильмы, приходящие, при загрузке страницы');
-    service.getPopularFilms().then(console.log);
+    service.getPopularFilms().then(data => {
+        if (data.total_results === 0) {
+            console.log('запросов не найдено')
+            return
+        };
+        console.log(data);
+    });
 };
 
 function resetMarkup() {
