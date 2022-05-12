adfb16a63426955c7443cc9f4c3f7bf0526ffbc5
fenota-viktoriya <Fen9206@gmail.com>
2022-05-12 18:44:42 +0300

    add pagination logic

--- src/js/API/api-service.js
+++ src/js/API/api-service.js
@@ -4,36 +4,35 @@ const API_KEY = '79fb62b7e77dc5ee41dd0c1332d74198';
 const BASE_URL = 'https://api.themoviedb.org';
 
 export default class {
-    constructor() {
-        this.service = axios.create({
-            baseURL: BASE_URL,
-            params: {
-                api_key: API_KEY,
-            },
-        });
+  constructor() {
+    this.service = axios.create({
+      baseURL: BASE_URL,
+      params: {
+        api_key: API_KEY,
+      },
+    });
 
-        this.options = {
-            urlPath: '',
-        };
+    this.options = {
+      urlPath: '',
     };
+  }
 
-    getFilms = async () => {
-        const response = await this.service.get(this.options.urlPath);
-        return response.data;
-    };
+  getFilms = async () => {
+    const response = await this.service.get(this.options.urlPath);
+    return response.data;
+  };
 
-    getFilmsByQuery(q) {
-    };
+  getFilmsByQuery(q) {}
 
-    resetUrlPath() {
-        this.options.urlPath = ''
-    };
+  resetUrlPath() {
+    this.options.urlPath = '';
+  }
 
-    get urlPath() {
-        return this.options.urlPath;
-    };
+  get urlPath() {
+    return this.options.urlPath;
+  }
 
-    set urlPath(newPath) {
-        this.options.urlPath = newPath;
-    };
-};
+  set urlPath(newPath) {
+    this.options.urlPath = newPath;
+  }
+}
--- src/js/tui.pagination/tui.pagination.js
+++ src/js/tui.pagination/tui.pagination.js
@@ -1,47 +1,39 @@
-import Pagination from 'tui-pagination';
+/*import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
 import { getFilmsByUrl } from '../render/main-render-logic';
 import ApiService from '../API/api-service';
 const service = new ApiService();
-import { refs } from '../render/refs';
+import refs from '../render/refs';
 
-const { formRef, inputRef, btnRef, containerRef } = refs();
-
-export function createPagination(totalItems, itemsPerPage, page, query) {
+export function createPagination(request, query) {
+  //request заполняется вручную и используется для логики
   const options = {
-    totalItems: totalItems,
-    itemsPerPage: itemsPerPage,
+    totalItems: 20000,
+    itemsPerPage: 20,
     visiblePages: 5,
-    page: page,
+    page: 1,
     centerAlign: true,
     firstItemClassName: 'tui-first-child',
-    template: {
-      page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-      currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-      moveButton:
-        '<a href="#" class="tui-page-btn tui-{{type}} custom-class-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
-        '</a>',
-      disabledMoveButton:
-        '<span class="tui-page-btn tui-is-disabled tui-{{type}} custom-class-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
-        '</span>',
-      moreButton:
-        '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip custom-class-{{type}}">' +
-        '<span class="tui-ico-ellip">...</span>' +
-        '</a>',
-    },
+    lastItemClassName: 'tui-last-child',
   };
-  const pagination = new Pagination(containerRef, options);
+
+  const pagination = new Pagination(refs.containerRef, options);
+
   if (request === 'query') {
     pagination.on('afterMove', event => {
       const currentPage = event.page;
-      //дальше делаем запрос на сервер с currentPage и query(что вводится в инпут) и отрисовываем разметку
+      service.getMoviesByQueryPagination(currentPage, query).then(r => console.log(r));
     });
   } else {
     pagination.on('afterMove', event => {
       const currentPage = event.page;
-      //дальше делаем запрос на сервер с currentPage и отрисовываем разметку
+      service.getPopularFilms(currentPage).then(r => console.log(r));
     });
   }
 }
+
+//getMoviesByQueryPagination(page, query) {
+/*return fetch(
+  `${BASE_URL}/search/movie?query=${query}&api_key=${API_KEY}&page=${page}&language=${langs}`,
+);
+*/
