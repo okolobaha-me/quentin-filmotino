5a91bab664230c71f234395d4c29bd30f903e91c
fenota-viktoriya <Fen9206@gmail.com>
2022-05-15 02:42:17 +0300

    add goolFonts/icon

--- src/js/render/main-render-logic.js
+++ src/js/render/main-render-logic.js
@@ -30,7 +30,7 @@ function onFormSubmit(e) {
     const markup = showMovies(data);
     refs.galleryRef.insertAdjacentHTML('beforeend', markup);
     showPagination(); // <========== подставить рендер renderFilmList(data)
-    createPagination(query);
+    createPagination(query, service.getTotalResults(data));
   });
 }
 
@@ -44,10 +44,9 @@ function onSiteLoad(e) {
       return;
     }
     console.log(data);
-    console.log(service.getTotalResults(data));
     const markup = showMovies(data);
     refs.galleryRef.insertAdjacentHTML('beforeend', markup); // <========== подставить рендер renderFilmList(data)
-    createPagination();
+    createPagination('', service.getTotalResults(data));
   });
 }
 
